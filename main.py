0173e6dada094d10626e3d512f4c7c8312a6d8a2
Rafael Costa <rafaeladriancordeirocosta@gmail.com>
2024-12-02 01:22:31 -0300

    Feat: Function to get username, password, negociation_id to init scraping and get data, to generate and print labels

---
+++ main.py
@@ -0,0 +1,20 @@
+"""Main module"""
+
+from scraping import CargaMaquinaClient
+from generate_labels import generate_nfe_labels
+from print_labels import print_labels
+
+
+def main() -> None:
+    """Main function"""
+    username: str = input("Usuário: ")
+    password: str = input("Senha: ")
+    negociation_id: str = input("ID da Negociação: ")
+    client = CargaMaquinaClient(username=username, password=password)
+    client.nfe_data_scraping(negociation_id)
+    generate_nfe_labels()
+    print_labels(["./pending_labels.pdf", "./stock_labels.pdf"])
+
+
+if __name__ == "__main__":
+    main()
